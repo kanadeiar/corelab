3cd3886f83f1a1eb3aff258ee8ad8e1fa3613c6c
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-06-23 23:22:51 +0400

    Первая глава

---
+++ ConsoleAppAsync/ConsoleAppAsync/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лаборатория асинхронного кода");
+
+            DumpWebPageAsync("https://yandex.ru/");
+
+            Console.Title = "Завершено";
+            Console.WriteLine("Нажмите любую кнопку для закрытия ...");
+            Console.ReadLine();
+        }
+
+        static async Task DumpWebPageAsync(string uri)
+        {
+            var client = new WebClient();
+            var page = await client.DownloadStringTaskAsync(uri);
+            Console.WriteLine(page);
+        }
+    }
+}
