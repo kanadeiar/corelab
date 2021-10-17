647631dc767debce64a98690169e8a5d98facae1
kanadeiar <kanadeiar@gmail.com>
2021-10-17 22:19:21 +0300

    Отображение итоговой суммы работает

--- MySolution/LanguageFeatures/Controllers/HomeController.cs
+++ MySolution/LanguageFeatures/Controllers/HomeController.cs
@@ -9,15 +9,29 @@ namespace LanguageFeatures.Controllers
     {
         public IActionResult Index()
         {
-            var results = new List<string>();
-            foreach (var p in Product.GetProducts())
+            // var results = new List<string>();
+            // foreach (var p in Product.GetProducts())
+            // {
+            //     var name = p?.Name ?? "<No Name>";
+            //     var price = p?.Price ?? 0;
+            //     var relatedName = p?.Related?.Name ?? "<None>";
+            //     results.Add($"Имя: {name}, Цена: {price:C2}, Подчинен: {relatedName}");
+            // }
+            var data = new object[] { 210M, 29.5M, "apple", "orange", 100, 10 };
+            decimal total = 0;
+            for (int i = 0; i < data.Length; i++)
             {
-                var name = p?.Name ?? "<No Name>";
-                var price = p?.Price ?? 0;
-                var relatedName = p?.Related?.Name ?? "<None>";
-                results.Add($"Имя: {name}, Цена: {price:C2}, Подчинен: {relatedName}");
+                switch (data[i])
+                {
+                    case decimal d:
+                        total += d;
+                        break;
+                    case int iVv when iVv > 50:
+                        total += iVv;
+                        break;
+                }
             }
-            return View(results);
+            return View(new string[] { $"Итог: {total:C2}" });
         }
     }
 }
