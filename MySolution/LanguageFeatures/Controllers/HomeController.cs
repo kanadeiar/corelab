f63ab7324112e87bac95f520ec8e684a8cd62d2f
kanadeiar <kanadeiar@gmail.com>
2021-10-17 21:35:23 +0300

    Вывод данных работает

--- MySolution/LanguageFeatures/Controllers/HomeController.cs
+++ MySolution/LanguageFeatures/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using LanguageFeatures.Model;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace LanguageFeatures.Controllers
 {
@@ -6,7 +9,15 @@ namespace LanguageFeatures.Controllers
     {
         public IActionResult Index()
         {
-            return View(new string[] { "C#", "Java", "C++" });
+            var results = new List<string>();
+            foreach (var p in Product.GetProducts())
+            {
+                var name = p?.Name ?? "<No Name>";
+                var price = p?.Price ?? 0;
+                var relatedName = p?.Related?.Name ?? "<None>";
+                results.Add($"Имя: {name}, Цена: {price:C2}, Подчинен: {relatedName}");
+            }
+            return View(results);
         }
     }
 }
--- MySolution/LanguageFeatures/Models/Product.cs
+++ MySolution/LanguageFeatures/Models/Product.cs
@@ -2,14 +2,16 @@ namespace LanguageFeatures.Model
 {
     public class Product
     {
-        public string Name { get; set; }
+        public string? Name { get; set; }
+        public string Category { get; set; } = "Спорт";
         public decimal? Price { get; set; }
-
+        public Product? Related { get; set; }
         public static Product[] GetProducts()
         {
-            var kayak = new Product { Name = "Каяк", Price = 275M };
+            var kayak = new Product { Name = "Каяк", Category = "Водный спорт", Price = 275M };
             var jacket = new Product { Name = "Жакет", Price = 48.95M };
-            return new Product[] { kayak, jacket, null };
+            kayak.Related = jacket;
+            return new Product[] { kayak, jacket, new Product() };
         }
     }
 }
