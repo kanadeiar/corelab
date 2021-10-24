20fdd9faa7323bb333eaae6deee97fd98d5d9b5d
kanadeiar <kanadeiar@gmail.com>
2021-10-24 09:45:36 +0300

    Асинхронная коллекция

--- MySolution/LanguageFeatures/Controllers/HomeController.cs
+++ MySolution/LanguageFeatures/Controllers/HomeController.cs
@@ -12,20 +12,12 @@ namespace LanguageFeatures.Controllers
     {
         public async Task<IActionResult> Index()
         {
-            //var output = new List<string>();
-            //await foreach (var len in MyAsyncMethods.GetPageLengthsAsync(output, "apress.com", "microsoft.com", "amazon.com"))
-            //{
-            //    output.Add($"Page length: {len}");
-            //}
-            //return View(output);
-            var products = new[]
+            var output = new List<string>();
+            await foreach (var len in MyAsyncMethods.GetPageLengthsAsync(output, "apress.com", "microsoft.com", "amazon.com"))
             {
-                new { Name = "Каяк", Price = 275M },
-                new { Name = "Флак", Price = 48.1M },
-                new { Name = "Черн", Price = 19.5M },
-                new { Name = "Посл", Price = 34.95M },
-            };
-            return View(products.Select(p => $"{nameof(p.Name)}: {p.Name}, {nameof(p.Price)}: {p.Price}"));
+                output.Add($"Page length: {len}");
+            }
+            return View(output);
         }
     }
 }
