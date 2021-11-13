b5b4614eaa76be201a9b394e8817599101579f15
kanadeiar <kanadeiar@gmail.com>
2021-11-14 00:25:55 +0300

    First project

---
+++ Firstproject/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿namespace Firstproject.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        int hour = DateTime.Now.Hour;
+        string viewModel = hour < 12 ? "Доброе утро" : "Добрый вечер";
+        return View("Index", viewModel);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
