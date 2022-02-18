6d26f4a8f2286242241d1b3647962bc9fa3b235c
kanadeiar <kanadeiar@gmail.com>
2022-02-18 19:03:57 +0300

    big clean

--- Firstproject/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Firstproject.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- PartyInvites/Global.cs
+++
@@ -1,7 +0,0 @@
-global using System;
-global using System.Collections.Generic;
-global using System.Linq;
-global using System.Threading.Tasks;
-global using System.Diagnostics;
-global using Microsoft.AspNetCore.Mvc;
-global using PartyInvites.Models;
