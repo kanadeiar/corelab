6d1aba20618cb93c9fbb2073a1de55a77b9d7564
kanadeiar <kanadeiar@gmail.com>
2022-02-26 18:38:05 +0300

    clear

--- Platform/Platform/MiddlewareOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Platform
-{
-    public class MiddlewareOptions
-    {
-        public string CityName { get; set; } = "New Yourk";
-        public string CountryName { get; set; } = "USA";
-    }
-}
--- Platform/Platform/Program.cs
+++ Platform/Platform/Program.cs
@@ -1,11 +1,5 @@
-using Platform;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Configure<MiddlewareOptions>(options =>
-{
-    options.CityName = "Albany";
-});
 
 var app = builder.Build();
 
@@ -13,41 +7,6 @@ if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
-
-app.UseMiddleware<LocationMiddleware>();
-
-app.Map("/branch", branch =>
-{
-    branch.Run(new QueryStringMiddleWare().Invoke);
-});
-
-app.Map("/time", appBuilder =>
-{
-    var time = DateTime.Now.ToShortTimeString();
-
-    appBuilder.Use(async (context, next) =>
-    {
-        Console.WriteLine($"Time: {time}");
-        await next();   
-    });
-
-    appBuilder.Run(async context => await context.Response.WriteAsync($"Time: {time}"));
-});
-
-app.Use(async (context, next) => {
-    await next();
-    await context.Response.WriteAsync($"\nCode: {context.Response.StatusCode}");
-});
-app.Use(async (context, next) => {
-    if (context.Request.Path == "/short")
-    {
-        await context.Response.WriteAsync("Short circuited");
-    }
-    else
-        await next();
-});
-
-app.UseMiddleware<QueryStringMiddleWare>();
 
 app.MapGet("/", () => "Hello World!");
 
