4b5a896e8a733f2f31a3ebc075b9f6e4017d5f97
kanadeiar <kanadeiar@gmail.com>
2022-02-26 10:17:11 +0300

    Middleware

---
+++ Platform/Platform/Middleware.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace Platform;
+
+public class QueryStringMiddleWare
+{
+    private RequestDelegate? next;
+
+    public QueryStringMiddleWare()
+    {
+        // do nothing
+    }
+
+    public QueryStringMiddleWare(RequestDelegate nextDelegate)
+    {
+        next = nextDelegate;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        if (context.Request.Method == HttpMethods.Get
+            )
+        {
+            if (!context.Response.HasStarted)
+            {
+                context.Response.ContentType = "text/plain";
+            }
+            await context.Response.WriteAsync("1 Class-based Middleware \n");
+        }
+        if (next != null)
+        {
+            await next(context);
+        }
+        await context.Response.WriteAsync("\n2 Class-based Middleware \n");
+    }
+}
+
+public class LocationMiddleware
+{
+    private readonly RequestDelegate _nextDelegate;
+    private readonly MiddlewareOptions _opts;
+    public LocationMiddleware(RequestDelegate nextDelegate, IOptions<MiddlewareOptions> opts)
+    {
+        _nextDelegate = nextDelegate;
+        _opts = opts.Value;
+    }
+    public async Task Invoke(HttpContext content)
+    {
+        if (content.Request.Path == "/location")
+        {
+            await content.Response.WriteAsync($"{_opts.CityName}, {_opts.CountryName}");
+        }
+        else
+        {
+            await _nextDelegate(content);
+        }
+    }
+}
---
+++ Platform/Platform/MiddlewareOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Platform
+{
+    public class MiddlewareOptions
+    {
+        public string CityName { get; set; } = "New Yourk";
+        public string CountryName { get; set; } = "USA";
+    }
+}
