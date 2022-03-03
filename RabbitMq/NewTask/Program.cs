797b409528f8c184f1a3380cb1089cca9fa4d6c2
kanadeiar <kanadeiar@gmail.com>
2022-03-03 21:43:43 +0300

    + queues

--- RabbitMq/NewTask/Program.cs
+++ RabbitMq/NewTask/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using RabbitMQ.Client;
+﻿using RabbitMQ.Client;
 using System.Text;
+using System.Threading.Channels;
 
-Console.WriteLine("RabbitMQ Application Send");
+Console.WriteLine("RabbitMQ Application NewTask");
 var factory = new ConnectionFactory() { HostName = "localhost" };
 using (var connection = factory.CreateConnection())
 using (var channel = connection.CreateModel())
@@ -12,14 +12,30 @@ using (var channel = connection.CreateModel())
         exclusive: false,
         autoDelete: false,
         arguments: null);
-    string message = "Hello World!";
+
+    var message = GetMessage(args);
     var body = Encoding.UTF8.GetBytes(message);
+
+    var prorerties = channel.CreateBasicProperties();
+    prorerties.Persistent = true;
+
     channel.BasicPublish(exchange: "",
-        routingKey: "hello",
-        basicProperties: null,
+        routingKey: "task_queue",
+        basicProperties: prorerties,
         body: body);
     Console.WriteLine(" [x] Send {0}", message);
 }
 
 Console.WriteLine("Press key to exit ...");
 Console.ReadKey();
+
+
+
+
+
+
+
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+}
--- RabbitMq/Worker/Program.cs
+++ RabbitMq/Worker/Program.cs
@@ -3,27 +3,40 @@ using RabbitMQ.Client;
 using System.Text;
 using RabbitMQ.Client.Events;
 
-Console.WriteLine("RabbitMQ Application Reveive");
+Console.WriteLine("RabbitMQ Application Worker");
 var factory = new ConnectionFactory() { HostName = "localhost" };
 using (var connection = factory.CreateConnection())
 using (var channel = connection.CreateModel())
 {
-    channel.QueueDeclare(queue: "hello",
+    channel.QueueDeclare(queue: "task_queue",
         durable: false,
         exclusive: false,
         autoDelete: false,
         arguments: null);
+
+    channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+    Console.WriteLine(" [*] Waiting for messages.");
+
     var consumer = new EventingBasicConsumer(channel);
     consumer.Received += (o, e) =>
     {
-        var body = e.Body.ToArray();
+        byte[] body = e.Body.ToArray();
         var message = Encoding.UTF8.GetString(body);
         Console.WriteLine(" [0] reveiced {0}", message);
+
+        int dots = message.Split('.').Length - 1;
+        Thread.Sleep(dots * 1000);
+
+        Console.WriteLine(" [x] Done");
+
+        channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
     };
-    channel.BasicConsume(queue: "hello",
-        autoAck: true,
+    channel.BasicConsume(queue: "task_queue",
+        autoAck: false,
         consumer: consumer);
     Console.WriteLine("Press key to exit ...");
     Console.ReadKey();
 }
+
 
