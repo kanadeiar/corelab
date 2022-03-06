24d82b6ddea220590a5f6c7958341413f8e1442e
kanadeiar <kanadeiar@gmail.com>
2022-03-06 21:04:13 +0300

    consent cookie

---
+++ Platform/Platform/ConsentMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+
+namespace Platform;
+
+public class ConsentMiddleware
+{
+    private readonly RequestDelegate _next;
+    public ConsentMiddleware(RequestDelegate nextDelegate)
+    {
+        _next = nextDelegate;
+    }
+    public async Task Invoke(HttpContext context)
+    {
+        if (context.Request.Path == "/consent")
+        {
+            var consentFeature = context.Features.Get<ITrackingConsentFeature>();
+            if (consentFeature.HasConsent)
+            {
+                consentFeature.WithdrawConsent();
+            }
+            else
+            {
+                consentFeature.GrantConsent();
+            }
+            await context.Response.WriteAsync(consentFeature.HasConsent ? "Consent Granted" : "Consent Withdrawn");
+        }
+        await _next(context);
+    }
+}
--- Platform/Platform/Program.cs
+++ Platform/Platform/Program.cs
@@ -1,9 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
-var servicesConfig = builder.Configuration;
-var servicesEnv = builder.Environment;
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.CheckConsentNeeded = context => true;
+});
 
-builder.Services.Configure<MessageOptions>(servicesConfig.GetSection("Location"));
+//var servicesConfig = builder.Configuration;
+//var servicesEnv = builder.Environment;
+//builder.Services.Configure<MessageOptions>(servicesConfig.GetSection("Location"));
+
+
 
 var app = builder.Build();
 
@@ -13,31 +19,62 @@ if (app.Environment.IsDevelopment())
     app.Logger.LogDebug("Start program pipeline");
 }
 
-var pipelineConfig = app.Configuration;
-var pipelineEnv = app.Environment;
+app.UseCookiePolicy();
+app.UseMiddleware<ConsentMiddleware>();
+app.UseRouting();
 
-app.UseHttpLogging();
+//var pipelineConfig = app.Configuration;
+//var pipelineEnv = app.Environment;
 
-app.UseStaticFiles();
+//app.UseHttpLogging();
 
-app.UseMiddleware<LocationMiddleware>();
+//app.UseStaticFiles();
 
-app.MapGet("config", async (HttpContext context, IConfiguration config, IWebHostEnvironment env) =>
+//app.UseMiddleware<LocationMiddleware>();
+
+//app.MapGet("config", async (HttpContext context, IConfiguration config, IWebHostEnvironment env) =>
+//{
+//    var defDebug = pipelineConfig["Logging:LogLevel:Default"];
+//    await context.Response.WriteAsync($"The config settings is {defDebug}\n");
+//    string environ = config["ASPNETCORE_ENVIRONMENT"];
+//    await context.Response.WriteAsync($"The env settings is: {environ}\n");
+//    await context.Response.WriteAsync($"The env is {env.EnvironmentName}\n");
+//    var wsId = config["WebService:Id"];
+//    var wsKey = config["WebService:Key"];
+//    await context.Response.WriteAsync($"\nThe secret Id is: {wsId}");
+//    await context.Response.WriteAsync($"\nThe secret Key is: {wsKey}");
+//});
+//app.MapGet("/", async context => 
+//{ 
+//    await context.Response.WriteAsync("Hello World!"); 
+//});
+
+app.MapGet("/cookie", async context =>
 {
-    var defDebug = pipelineConfig["Logging:LogLevel:Default"];
-    await context.Response.WriteAsync($"The config settings is {defDebug}\n");
-    string environ = config["ASPNETCORE_ENVIRONMENT"];
-    await context.Response.WriteAsync($"The env settings is: {environ}\n");
-    await context.Response.WriteAsync($"The env is {env.EnvironmentName}\n");
-    var wsId = config["WebService:Id"];
-    var wsKey = config["WebService:Key"];
-    await context.Response.WriteAsync($"\nThe secret Id is: {wsId}");
-    await context.Response.WriteAsync($"\nThe secret Key is: {wsKey}");
+    var counter1 = int.Parse(context.Request.Cookies["counter1"] ?? "0") + 1;
+    context.Response.Cookies.Append("counter1", counter1.ToString(), 
+        new CookieOptions 
+        { 
+            MaxAge = TimeSpan.FromMinutes(30),
+            IsEssential = true,
+        });
+    var counter2 = int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
+    context.Response.Cookies.Append("counter2", counter2.ToString(), 
+        new CookieOptions 
+        { 
+            MaxAge = TimeSpan.FromMinutes(30) 
+        });
+    await context.Response.WriteAsync($"Counter1: {counter1}, counter2: {counter2}");
+});
+app.MapGet("/clear", context => 
+{
+    context.Response.Cookies.Delete("counter1");
+    context.Response.Cookies.Delete("counter2");
+    context.Response.Redirect("/");
+    return Task.CompletedTask;
 });
 
-app.MapGet("/", async context => 
-{ 
-    await context.Response.WriteAsync("Hello World!"); 
-});
+app.MapFallback(async context => 
+    await context.Response.WriteAsync("Hello World!"));
 
 app.Run();
