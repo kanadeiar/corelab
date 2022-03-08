189f57155c63d8e3bccc5f52bfe22463138cab79
kanadeiar <kanadeiar@gmail.com>
2022-03-08 08:55:54 +0300

    request

--- RabbitMq/Request/Controllers/HomeController.cs
+++ RabbitMq/Request/Controllers/HomeController.cs
@@ -1,21 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
 using Request.Models;
 using System.Diagnostics;
+using Test.Core.Contracts;
+using Test.Core.Models;
 
 namespace Request.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IRequestClient<ProductInfoRequest> _client;
+        public HomeController(ILogger<HomeController> logger, IRequestClient<ProductInfoRequest> client)
         {
             _logger = logger;
+            _client = client;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Myrequest()
+        {
+            Product model = null;
+
+            using (var request = _client.Create(new ProductInfoRequest { Slug = "test" }))
+            {
+                var response = await request.GetResponse<ProductInfoResponse>();
+                model = response.Message.Product;
+            }
+
+            return View(model);
         }
 
         public IActionResult Privacy()
--- RabbitMq/Request/Program.cs
+++ RabbitMq/Request/Program.cs
@@ -1,15 +1,32 @@
-var builder = WebApplication.CreateBuilder(args);
+using MassTransit;
+using Test.Core.Contracts;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+var builder = WebApplication.CreateBuilder(args);
+var host = builder.Configuration["MassTransit:Host"];
+
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+builder.Services.AddMassTransit(x => 
+{
+    x.AddBus(context => Bus.Factory.CreateUsingRabbitMq(c =>
+    {
+        c.Host(host);
+        c.ConfigureEndpoints(context);
+    }));
+
+    x.AddRequestClient<ProductInfoRequest>();
+});
+builder.Services.AddMassTransitHostedService();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
