f61ad6176e1b637270eb11c280cf66348155cc1b
kanadeiar <kanadeiar@gmail.com>
2022-03-10 22:36:01 +0300

    + controller

---
+++ Platform/WebApp/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [Route("api/{controller}")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return new Product[] { new Product { Name = "Test1" }, new Product { Name = "Test2" } };
+        }
+        [HttpGet("{id}")]
+        public Product GetProduct(int id)
+        {
+            return new Product { Name = string.Concat("Test_", id.ToString()) };
+        }
+    }
+}
--- Platform/WebApp/Program.cs
+++ Platform/WebApp/Program.cs
@@ -16,58 +16,16 @@ builder.Host.ConfigureServices(services =>
     });
 });
 
+builder.Services.AddMvc();
+
 var app = builder.Build();
-const string baseUrl = "api/product";
 
-app.MapGet(baseUrl, async (HttpContext context, DataContext data) => 
-{
-    context.Response.ContentType = "application/json";
-    await context.Response.WriteAsync(JsonSerializer.Serialize<IEnumerable<Product>>(data.Products));
-});
+app.UseDeveloperExceptionPage();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseMiddleware<TestMiddleware>();
 
-app.MapGet($"{baseUrl}/{{id}}", async (HttpContext context, DataContext data) =>
-{
-    var id = context.Request.RouteValues["id"] as string;
-    if (id is { })
-    {
-        var product = data.Products.Find(int.Parse(id));
-        if (product is { })
-        {
-            context.Response.ContentType="application/json";
-            await context.Response.WriteAsync(JsonSerializer.Serialize<Product>(product));
-        }
-        else
-        {
-            context.Response.StatusCode = StatusCodes.Status404NotFound;
-        }
-    }
-    else
-    {
-        context.Response.StatusCode = StatusCodes.Status400BadRequest;
-    }
-});
-
-app.MapPost($"{baseUrl}", async (HttpContext context, DataContext data) => 
-{
-    var product = await JsonSerializer.DeserializeAsync<Product>(context.Request.Body);
-    //var product = new Product { Name = "Test", Price = 12.2M, CategoryId = 1, SupplierId = 1 };
-    if (product is { })
-    {
-        data.Add(product);
-        await data.SaveChangesAsync();
-        context.Response.StatusCode = StatusCodes.Status200OK;
-    }
-    else
-    {
-        context.Response.StatusCode = StatusCodes.Status404NotFound;
-    }
-});
-
-//app.UseDeveloperExceptionPage();
-//app.UseStaticFiles();
-//app.UseRouting();
-
-//app.UseMiddleware<TestMiddleware>();
+app.MapControllers();
 
 app.MapGet("/", () => "Hello World!");
 
