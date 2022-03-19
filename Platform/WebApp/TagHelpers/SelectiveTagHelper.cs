5657882a1e498a4cbb180e38bae847b9e074b6de
kanadeiar <kanadeiar@gmail.com>
2022-03-20 01:48:35 +0300

    if-tag-helper

---
+++ Platform/WebApp/TagHelpers/SelectiveTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebApp.TagHelpers;
+
+[HtmlTargetElement("div", Attributes = "max-product-id, for")]
+public class SelectiveTagHelper : TagHelper
+{
+    public int MaxProductId { get; set; }
+    public ModelExpression For { get; set; }
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        if (For.Model.GetType() == typeof(int) && (int)For.Model < MaxProductId)
+        {
+            output.SuppressOutput();
+        }
+    }
+}
