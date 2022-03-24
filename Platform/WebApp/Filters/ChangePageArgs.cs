2c9f010f9b8cc6b66332f391a3bfd20460650e18
kanadeiar <kanadeiar@gmail.com>
2022-03-25 00:00:08 +0300

    page filter

---
+++ Platform/WebApp/Filters/ChangePageArgs.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApp.Filters;
+
+public class ChangePageArgs : Attribute, IPageFilter
+{
+    public void OnPageHandlerExecuted(PageHandlerExecutedContext context)
+    {        
+    }
+
+    public void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+    {
+        if (context.HandlerArguments.ContainsKey("message1"))
+        {
+            context.HandlerArguments["message1"] = "NewMessage1";
+        }
+    }
+
+    public void OnPageHandlerSelected(PageHandlerSelectedContext context)
+    {        
+    }
+}
