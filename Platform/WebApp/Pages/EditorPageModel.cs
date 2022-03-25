596b3d58170f3e442f697c16d450910258728265
kanadeiar <kanadeiar@gmail.com>
2022-03-26 02:57:37 +0300

    Pre - crud pages

---
+++ Platform/WebApp/Pages/EditorPageModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Pages;
+
+public class EditorPageModel : PageModel
+{
+    public readonly DataContext DataContext;
+    public ProductWebModel WebModel { get; set; }
+    public IEnumerable<Category> Categories => DataContext.Categiries;
+    public IEnumerable<Supplier> Suppliers => DataContext.Suppliers;
+    public EditorPageModel(DataContext dataContext)
+    {
+        DataContext = dataContext;
+    }
+}
