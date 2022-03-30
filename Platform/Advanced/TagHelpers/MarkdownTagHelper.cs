dab4a14c56ac3155886ed7a3d628805f8b440db4
kanadeiar <kanadeiar@gmail.com>
2022-03-30 17:50:49 +0300

    + Markdown

---
+++ Platform/Advanced/TagHelpers/MarkdownTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using HeyRed.MarkdownSharp;
+
+namespace Advanced.TagHelpers;
+
+[HtmlTargetElement("p", Attributes = "markdown")]
+public class MarkdownTagHelper : TagHelper
+{
+    public string Markdown { get; set; }
+    public async override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+        var options = new MarkdownOptions
+        {
+            AutoHyperlink = true,
+            AutoNewLines = true,
+            LinkEmails = true,
+            QuoteSingleLine = true,
+            StrictBoldItalic = true
+        };
+        Markdown mark = new Markdown(options);
+        var html = mark.Transform(Markdown);
+        output.Content.AppendHtml(html);
+    }
+}
