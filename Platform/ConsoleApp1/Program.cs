28c3c51e61fcc0d7c65a6c28a351f69598e0a705
kanadeiar <kanadeiar@gmail.com>
2022-05-21 14:26:24 +0300

    Чистка кода

--- Platform/ConsoleApp1/Program.cs
+++ Platform/ConsoleApp1/Program.cs
@@ -1,73 +1,24 @@
-﻿internal delegate void Feedback(int value);
+﻿using System.Diagnostics.Contracts;
 
 public sealed partial class Program {
     public static void Main()
     {
-        StaticDelegate();
-        InstanceDelegate();
-        Console.WriteLine("- Chain");
-        ChainDelegate(new Program());
+        var val = Sum(1, -1);
+        Console.WriteLine(val);
         Console.WriteLine("Hello, World!");
         Console.ReadKey(true);
     }
 
-    public static void StaticDelegate()
+    static int Sum(int a, int b)
     {
-        Counter(1, 3, null);
-        Counter(1, 3, new Feedback(FeedbackToConsole));
-        Console.WriteLine();
-    }
-
-    public static void InstanceDelegate()
-    {
-        var p = new Program();
-        Counter(1, 3, new Feedback(p.FeedbackToInstance));
-        Console.WriteLine();
-    }
-
-    public static void ChainDelegate(Program p)
-    {
-        var fb1 = new Feedback(FeedbackToConsole);
-        var fb2 = new Feedback(Feedback2ToConsole);
-        var fb3 = new Feedback(p.FeedbackToInstance);
-
-        Feedback chain = null;
-        chain += fb1;
-        chain += fb2;
-        chain += fb3;
-        Counter(1, 3, chain);
-        Console.WriteLine(Environment.NewLine + "- Rechain");
-        chain -= new Feedback(FeedbackToConsole);
-        chain -= new Feedback(Feedback2ToConsole);
-        Counter(1, 3, chain);
-    }
-
-    static void Counter(int from, int to, Feedback fb)
-    {
-        for (int i = from; i < to; i++)
-        {
-            if (fb != null)
-            {
-                fb.Invoke(i);
-            }
-        }
-    }
-
-    static void FeedbackToConsole(int value)
-    {
-        Console.WriteLine("Item=" + value);
-    }
-
-    static void Feedback2ToConsole(int value)
-    {
-        Console.WriteLine("Item2=" + value);
-    }
-
-    void FeedbackToInstance(int value)
-    {
-        Console.WriteLine("ItemInst=" + value);
+        Contract.Requires(a > 0, "Нужно чтоб а было больше нуля");
+        Contract.Requires(b > 0, "Нужно чтоб b было больше нуля");
+        
+        var value = a + b;
+        return value;
     }
 }
+
 
 
 
