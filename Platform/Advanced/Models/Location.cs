79cd6bf2a5a854896931766203dadb9e15735128
kanadeiar <kanadeiar@gmail.com>
2022-07-17 23:19:28 +0300

    Очистка

--- Platform/Advanced/Models/Location.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Advanced.Models;
-
-public class Location
-{
-    public int Id { get; set; }
-    public string State { get; set; }
-    public string City { get; set; }
-    public IEnumerable<Person> Persons { get; set; } 
-}
