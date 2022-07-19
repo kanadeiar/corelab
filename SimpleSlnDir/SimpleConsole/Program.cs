6aadbc245504eb74b1c59e1c798aa81d1df2a396
kanadeiar <kanadeiar@gmail.com>
2022-07-19 20:26:13 +0300

    First application

---
+++ SimpleSlnDir/SimpleConsole/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Title = "Test";
+var name = "Andrey";
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.DarkGray;
+Console.WriteLine("Hello, World!");
+Console.WriteLine("**************************");
+Console.WriteLine($"Name: {name}");
+Console.BackgroundColor = ConsoleColor.Black;
+var _ = Console.ReadKey();
