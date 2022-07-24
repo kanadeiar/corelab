7f7e366e8fcd9a615aaba53301cad29e8e3640b6
kanadeiar <kanadeiar@gmail.com>
2022-07-24 08:27:18 +0300

    Очистка

--- SimpleSlnDir/SimpleConsole/Program.cs
+++ SimpleSlnDir/SimpleConsole/Program.cs
@@ -1,9 +1,6 @@
-﻿Console.Title = "Test";
-var name = "Andrey";
-Console.BackgroundColor = ConsoleColor.Yellow;
-Console.ForegroundColor = ConsoleColor.DarkGray;
-Console.WriteLine("Hello, World!");
-Console.WriteLine("**************************");
-Console.WriteLine($"Name: {name}");
-Console.BackgroundColor = ConsoleColor.Black;
+﻿
+
+Console.WriteLine("Нажмите любую кнопку ...");
 var _ = Console.ReadKey();
+
+
