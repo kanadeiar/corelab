9a40f162e466d2645787407704d76d8baafc9a58
kanadeiar <kanadeiar@gmail.com>
2022-07-27 07:43:02 +0300

    Изменения

--- SimpleSlnDir/SimpleConsole/Program.cs
+++ SimpleSlnDir/SimpleConsole/Program.cs
@@ -1,6 +1,11 @@
 ﻿
-
-Console.WriteLine("Нажмите любую кнопку ...");
-var _ = Console.ReadKey();
-
-
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        System.Console.WriteLine("Заголовок программы");
+        await Task.Delay(3000);
+        Console.WriteLine("Нажмите любую кнопку ...");
+        var _ = Console.ReadKey();
+    }
+}
