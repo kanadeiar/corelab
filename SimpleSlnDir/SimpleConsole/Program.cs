5d7d716dac0114d1755ac9ff4474c07750cdba6f
kanadeiar <kanadeiar@gmail.com>
2022-08-05 08:15:55 +0300

    Интернирование

--- SimpleSlnDir/SimpleConsole/Program.cs
+++ SimpleSlnDir/SimpleConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Diagnostics;
+using System.Globalization;
 using Kanadeiar.Core.Tools;
 
 namespace CalculatorExamples;
@@ -9,10 +10,12 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-
+        string s1 = "Hello";
+        string s2 = "Hello";
+        Console.WriteLine(object.ReferenceEquals(s1, s2));
 
         Console.WriteLine("Нажмите любую кнопку ...");
-        var _ = Console.ReadKey();
+        Console.ReadKey();
     }
 }
 
