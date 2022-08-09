5f24868f5eba42702b60a532e5420808bafd48ea
kanadeiar <kanadeiar@gmail.com>
2022-08-10 01:15:48 +0300

    Изменения

--- SimpleSlnDir/SimpleConsole/Program.cs
+++ SimpleSlnDir/SimpleConsole/Program.cs
@@ -1,5 +1,8 @@
 ﻿
 
+Action<int> action1;
+Func<int, int> func1;
+
 int? val = 5;
 int val2 = val ?? 4;
 
@@ -13,9 +16,26 @@ var valuesWithNames2 = (FirstLetter: "a", TheNumber: 5, SecondLetter: "c");
 string[] strs = { "1", "2", "3", "4", "5" };
 var elem = strs[^2];
 
+// использование
+SampleVal v1 = new();
+var v2 = new SampleVal(10);
 
 Console.WriteLine("Нажмите любую кнопку ...");
 var v = Console.Read();
 
+internal struct SampleVal
+{
+    public int value = 10;
+    public int Val2 { get; set; }
+    public SampleVal()
+    {
+        Val2 = 20;
+    }
+    public SampleVal(Int32 value)
+    {
+        this = new SampleVal();
+        value = 10;
+    }
+}
 
 
--- SimpleSlnDir/SimpleConsole/Sample.cs
+++ SimpleSlnDir/SimpleConsole/Sample.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace SimpleConsole;
 
 public class Sample
 {
-    public string? Name { get; set; } = null;
+    public string Name { get; set; }
     public void Test(string name)
     {
         ArgumentNullException.ThrowIfNull(name);
