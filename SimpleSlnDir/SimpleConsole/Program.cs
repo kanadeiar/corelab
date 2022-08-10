f210afb9ca8122b6264b3fdbe4b606dada7edd4f
kanadeiar <kanadeiar@gmail.com>
2022-08-11 00:13:52 +0300

    Программы

--- SimpleSlnDir/SimpleConsole/Program.cs
+++ SimpleSlnDir/SimpleConsole/Program.cs
@@ -1,11 +1,24 @@
-﻿
+﻿using SimpleConsole;
 
-var dict = new Resources<int>();
-dict.Dict.Add("1", new Resource<int>());
+var rec = new SampleRec(10, true);
+var same = rec;
+var copy = rec with { };
+var two = rec with { Value = 10 };
 
 
 
+var resources = new Resources();
 
+var ints = resources.Set<int>();
+ints.Add("1", new Resource<int> { MyValue = 1 });
+ints.Add("2", new Resource<int> { MyValue = 2 });
+ints.Add("3", new IntResource { MyValue = 4 });
+foreach (var e in ints)
+{
+    Console.WriteLine($"{e.Key} {e.Value.MyValue}");
+}
+resources.Set<double>().Add("1", new Resource<double>());
+resources.Set<string>().Add("1", new Resource<string>());
 
 
 
@@ -15,14 +28,38 @@ var v = Console.Read();
 
 public interface IResource<T>
 {
+    public string Uri { get; set; }
     public T MyValue { get; set; }
 }
 public class Resource<T> : IResource<T>
 {
+    public string Uri { get; set; }
     public T MyValue { get; set; }
 }
-
-public class Resources<T>
+public class IntResource : Resource<int>, IResource<int>
 {
-    public IDictionary<string, IResource<T>> Dict { get; } = new Dictionary<string, IResource<T>>();
+    public string Uri { get; set; }
+    public int MyValue { get; set; }
+}
+
+public class Resources
+{
+    public IDictionary<string, IResource<int>> Dict { get; } = new Dictionary<string, IResource<int>>();
+    public IDictionary<string, IResource<double>> Dict2 { get; } = new Dictionary<string, IResource<double>>();
+    public IDictionary<string, IResource<string>> Dict3 { get; } = new Dictionary<string, IResource<string>>();
+
+    public IDictionary<string, IResource<T>> Set<T>()
+    {
+        switch (Type.GetTypeCode(typeof(T)))
+        {
+            case TypeCode.Int32:
+                return (IDictionary<string, IResource<T>>)Dict;
+            case TypeCode.Double:
+                return (IDictionary<string, IResource<T>>)Dict2;
+            case TypeCode.String:
+                return (IDictionary<string, IResource<T>>)Dict3;
+            default:
+                return null;
+        }
+    }
 }
--- SimpleSlnDir/SimpleConsole/Sample.cs
+++ SimpleSlnDir/SimpleConsole/Sample.cs
@@ -1,13 +1,34 @@
 namespace SimpleConsole;
 
-public class Sample
+// public class Sample
+// {
+//     public string Name { get; set; } = default!;
+//     public void Test(string name)
+//     {
+//         ArgumentNullException.ThrowIfNull(name);
+//         // код
+//     }
+// }
+
+internal sealed class Sample
 {
-    public string Name { get; set; } = default!;
-    public void Test(string name)
+    public int Value { get; init; }
+    public Sample(int val)
     {
-        ArgumentNullException.ThrowIfNull(name);
-        // код
+        Value = val;
     }
 }
+
+internal struct SampleVal
+{
+    public SampleVal(out int val)
+    {
+        val = 10;
+    }
+}
+
+record SampleRec(int Value, bool It);
+record class SampleRecClass(int Value, bool It) : SampleRec(Value, It);
+readonly record struct SampleRecStruct(int Value, bool It);
 
 
