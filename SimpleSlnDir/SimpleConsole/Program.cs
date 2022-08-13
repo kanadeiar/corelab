d9d7f48237122c381286a70b2d186d85400a8d93
kanadeiar <kanadeiar@gmail.com>
2022-08-13 08:43:49 +0300

    Продолжение

--- SimpleSlnDir/SimpleConsole/Program.cs
+++ SimpleSlnDir/SimpleConsole/Program.cs
@@ -1,65 +1,31 @@
-﻿using SimpleConsole;
-
-var rec = new SampleRec(10, true);
-var same = rec;
-var copy = rec with { };
-var two = rec with { Value = 10 };
+﻿namespace SimpleConsole;
 
 
 
-var resources = new Resources();
-
-var ints = resources.Set<int>();
-ints.Add("1", new Resource<int> { MyValue = 1 });
-ints.Add("2", new Resource<int> { MyValue = 2 });
-ints.Add("3", new IntResource { MyValue = 4 });
-foreach (var e in ints)
+internal class Program
 {
-    Console.WriteLine($"{e.Key} {e.Value.MyValue}");
-}
-resources.Set<double>().Add("1", new Resource<double>());
-resources.Set<string>().Add("1", new Resource<string>());
-
-
-
-Console.WriteLine("Нажмите любую кнопку ...");
-var v = Console.Read();
-
-
-public interface IResource<T>
-{
-    public string Uri { get; set; }
-    public T MyValue { get; set; }
-}
-public class Resource<T> : IResource<T>
-{
-    public string Uri { get; set; }
-    public T MyValue { get; set; }
-}
-public class IntResource : Resource<int>, IResource<int>
-{
-    public string Uri { get; set; }
-    public int MyValue { get; set; }
-}
-
-public class Resources
-{
-    public IDictionary<string, IResource<int>> Dict { get; } = new Dictionary<string, IResource<int>>();
-    public IDictionary<string, IResource<double>> Dict2 { get; } = new Dictionary<string, IResource<double>>();
-    public IDictionary<string, IResource<string>> Dict3 { get; } = new Dictionary<string, IResource<string>>();
-
-    public IDictionary<string, IResource<T>> Set<T>()
+    private static void Main(string[] args)
     {
-        switch (Type.GetTypeCode(typeof(T)))
+        unsafe
         {
-            case TypeCode.Int32:
-                return (IDictionary<string, IResource<T>>)Dict;
-            case TypeCode.Double:
-                return (IDictionary<string, IResource<T>>)Dict2;
-            case TypeCode.String:
-                return (IDictionary<string, IResource<T>>)Dict3;
-            default:
-                return null;
+            int val = 2;
+            Square(&val);
+            Console.WriteLine(val);
+        }
+
+
+        Console.WriteLine("Нажмите любую кнопку ...");
+        var v = Console.Read();
+
+        static unsafe void Square(int* pointer)
+        {
+            *pointer *= *pointer;
         }
     }
+}
+
+unsafe struct NodeVal
+{
+    public int Value;
+    public NodeVal* Next;
 }
