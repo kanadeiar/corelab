12bd3ed6a9abd3e542c2ff5b22ccb62d1a964b5f
kanadeiar <kanadeiar@gmail.com>
2022-08-13 08:45:07 +0300

    Очистка

--- SimpleSlnDir/SimpleConsole/Program.cs
+++ SimpleSlnDir/SimpleConsole/Program.cs
@@ -1,31 +1,11 @@
-﻿namespace SimpleConsole;
+﻿
 
 
 
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        unsafe
-        {
-            int val = 2;
-            Square(&val);
-            Console.WriteLine(val);
-        }
 
 
-        Console.WriteLine("Нажмите любую кнопку ...");
-        var v = Console.Read();
 
-        static unsafe void Square(int* pointer)
-        {
-            *pointer *= *pointer;
-        }
-    }
-}
+Console.WriteLine("Нажмите любую кнопку ...");
+var v = Console.Read();
 
-unsafe struct NodeVal
-{
-    public int Value;
-    public NodeVal* Next;
-}
+
