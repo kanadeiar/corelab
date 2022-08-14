08a85e0163839ccc297d8f474c69cd2f87ec3053
kanadeiar <kanadeiar@gmail.com>
2022-08-14 12:54:53 +0300

    async

--- SimpleSlnDir/SimpleConsole/Program.cs
+++ SimpleSlnDir/SimpleConsole/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace SimpleConsole;
+﻿using System.Reflection;
+
+namespace SimpleConsole;
 
 
 
@@ -8,10 +10,24 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-
+        Console.WriteLine("Test");
+        var s = new Sample();
+        var result = Task.Run(() => s.Method(5)).Result;
+        Console.WriteLine(result);
 
         Console.WriteLine("Нажмите любую кнопку ...");
         Console.ReadKey();
+    }
+
+}
+
+class Sample
+{
+    public async Task<int> Method(int value)
+    {
+        int foo = value * 2;
+        await Task.Delay(1000);
+        return foo;
     }
 }
 
