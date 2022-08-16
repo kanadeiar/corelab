f5495ee7bbfcf1f474900d47df865d5b2c59582b
kanadeiar <kanadeiar@gmail.com>
2022-08-16 08:03:03 +0300

    Асинхронность

--- SimpleSlnDir/SimpleConsole/Program.cs
+++ SimpleSlnDir/SimpleConsole/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
+using System.Text;
 
 namespace SimpleConsole;
+
 
 
 
@@ -10,23 +12,45 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        ThreadPool.QueueUserWorkItem(Method, 5);
-        new Task(Method, 5).Start();
-        Task.Run(() => Method(5));
+        var parent = new Task(() =>
+        {
+            var cts = new CancellationTokenSource();
+            var tf = new TaskFactory<int>(cts.Token, TaskCreationOptions.AttachedToParent, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
+            var childs = new Task<int>[]
+            {
+                tf.StartNew(() => Sum(100, cts.Token)),
+                tf.StartNew(() => Sum(200, cts.Token)),
+                tf.StartNew(() => Sum(150, cts.Token)),
+            };
+            for (int i = 0; i < childs.Length; i++)
+            {
+                childs[i].ContinueWith(x => cts.Cancel(), TaskContinuationOptions.OnlyOnFaulted);
+            }
+            tf.ContinueWhenAll(childs, x => x.Where(t => !t.IsFaulted && !t.IsCanceled).Max(t => t.Result), CancellationToken.None)
+            .ContinueWith(t => Console.WriteLine("Максимальное: {0}", t.Result), TaskContinuationOptions.ExecuteSynchronously);
+        });
+        parent.ContinueWith(p =>
+        {
+            var sb = new StringBuilder("Обнаружены исключения:" + Environment.NewLine);
+            foreach (var e in p.Exception.Flatten().InnerExceptions)
+                sb.AppendLine(" " + e.GetType().ToString());
+            Console.WriteLine(sb.ToString());
+        }, TaskContinuationOptions.OnlyOnFaulted);
+        parent.Start();
 
-        Console.WriteLine("Любую кнопку нажать для отмены");
-        Console.ReadKey();
-        
-        
-        
         Console.WriteLine("Нажмите любую кнопку ...");
-        Console.ReadKey();
+        var _ = Console.Read();
     }
 
-
-    static void Method(object? state)
+    static int Sum(int values, CancellationToken? token = null)
     {
-        Console.WriteLine("Готово !");
+        var sum = 0;
+        for (; values > 0; values--)
+        {
+            token?.ThrowIfCancellationRequested();
+            sum += values;
+        }
+        return sum;
     }
 }
 
