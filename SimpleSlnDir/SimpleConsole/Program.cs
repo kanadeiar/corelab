b2b70d7d2b91b8bf0a4eb402e909015ab27a472d
kanadeiar <kanadeiar@gmail.com>
2022-08-23 01:22:49 +0300

    Асинхронность

--- SimpleSlnDir/SimpleConsole/Program.cs
+++ SimpleSlnDir/SimpleConsole/Program.cs
@@ -2,14 +2,32 @@
 
 internal partial class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
+        Console.WriteLine("Начало программы");
+        var asyncLock = new AsyncLock();
 
+        await SimpleStatic.AddAsync(asyncLock);
 
         Console.WriteLine("Нажмите любую кнопку ...");
         var _ = Console.Read();
     }
 }
+
+static class SimpleStatic
+{
+    private static int _value;
+    public static int Value => _value;
+    public static async Task AddAsync(AsyncLock asyncLock)
+    {
+        await asyncLock.WaitAsync(LockMode.Shared); // Запросить общий доступ
+        Console.WriteLine(_value); // Чтение из ресурса...
+        asyncLock.Release(); // Снятие блокировки, чтобы ресурс стал доступен другим потокам
+    }
+}
+
+
+
 
 
 
