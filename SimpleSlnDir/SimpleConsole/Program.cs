329546e9a037678b7cad0f5f5ce1199117414641
kanadeiar <kanadeiar@gmail.com>
2022-08-23 02:08:22 +0300

    Асинх

--- SimpleSlnDir/SimpleConsole/Program.cs
+++ SimpleSlnDir/SimpleConsole/Program.cs
@@ -7,26 +7,35 @@ internal partial class Program
     private static async Task Main(string[] args)
     {
         Console.WriteLine("Начало программы");
-        var asyncLock = new AsyncLock();
 
-        await SimpleStatic.AddAsync(asyncLock);
+        await foreach (var e in GetInts())
+        {
+            Console.WriteLine(e);
+        }
+
 
         Console.WriteLine("Нажмите любую кнопку ...");
         var _ = Console.Read();
     }
-}
 
-static class SimpleStatic
-{
-    private static int _value;
-    public static int Value => _value;
-    public static async Task AddAsync(AsyncLock asyncLock)
+    static async IAsyncEnumerable<int> GetInts()
     {
-        await asyncLock.WaitAsync(LockMode.Shared); // Запросить общий доступ
-        Console.WriteLine(_value); // Чтение из ресурса...
-        asyncLock.Release(); // Снятие блокировки, чтобы ресурс стал доступен другим потокам
+        for (var i = 0; i < 10; i++)
+        {
+            await Task.Delay(100);
+            yield return i;
+        }
+    }
+
+    private static async ValueTask<int> GetValue(int value)
+    {
+        var result = value * 2;
+        await Task.Delay(1000);
+        return result;
     }
 }
+
+
 
 
 
