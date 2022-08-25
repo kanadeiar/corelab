35a8250f91710d4f384f292f7d8ac5c85e1f295a
kanadeiar <kanadeiar@gmail.com>
2022-08-25 19:10:31 +0300

    Очистка

--- SimpleSlnDir/SimpleConsole/Program.cs
+++ SimpleSlnDir/SimpleConsole/Program.cs
@@ -8,29 +8,10 @@ internal partial class Program
     {
         Console.WriteLine("Начало программы");
 
-        await foreach (var e in GetInts())
-        {
-            Console.WriteLine(e);
-        }
+
 
         Console.WriteLine("Нажмите любую кнопку ...");
         var _ = Console.Read();
-    }
-
-    static async IAsyncEnumerable<int> GetInts()
-    {
-        for (var i = 0; i < 10; i++)
-        {
-            await Task.Delay(100);
-            yield return i;
-        }
-    }
-
-    private static async ValueTask<int> GetValue(int value)
-    {
-        var result = value * 2;
-        await Task.Delay(1000);
-        return result;
     }
 }
 
