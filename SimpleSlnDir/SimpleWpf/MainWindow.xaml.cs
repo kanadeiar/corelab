57c57c05549194ab0d0d2d03f6d13c7fa68d2c9b
kanadeiar <kanadeiar@gmail.com>
2022-08-29 16:17:28 +0300

    Изменения

--- SimpleSlnDir/SimpleWpf/MainWindow.xaml.cs
+++ SimpleSlnDir/SimpleWpf/MainWindow.xaml.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Drawing;
 using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,14 +18,99 @@ namespace SimpleWpf;
 
 public partial class MainWindow : Window
 {
+    readonly IList<Inventory> cars = new List<Inventory>(); //наблюдаемая коллекция
+
     public MainWindow()
     {
         InitializeComponent();
+        cars.Add(new Inventory
+        {
+            CarId = 1,
+            Make = "УАЗ",
+            Color = "Белый",
+            PetName = "Буханка",
+        });
+        cars.Add(new Inventory
+        {
+            CarId = 2,
+            Make = "ЗАЗ",
+            Color = "Коричневый",
+            PetName = "Большая"
+        });
+        comboBoxCars.ItemsSource = cars;
     }
-
-    private void ButtonMy_OnClick(object sender, RoutedEventArgs e)
+    private void ButtonChangeColor_OnClick(object sender, RoutedEventArgs e)
     {
-        MessageBox.Show("Щелчок по необычной кнопке");
+        cars.First<Inventory>(x => x.CarId == ((Inventory)comboBoxCars.SelectedItem)?.CarId).Color = "Pink";
+    }
+    private void buttonAddCar_Click(object sender, RoutedEventArgs e)
+    {
+        var maxCount = cars?.Max(x => x.CarId) ?? 0;
+        cars.Add(new Inventory
+        {
+            CarId = maxCount + 1,
+            Color = "Желтый",
+            Make = "ВАЗ",
+            PetName = "Копейка",
+        });
+    }
+}
+
+class Inventory : INotifyPropertyChanged //наблюдаемая модель
+{
+    private int carId;
+    public int CarId
+    {
+        get => carId;
+        set
+        {
+            if (value == carId) return;
+            carId = value;
+            //передача названия свойства значение которого изменилось
+            OnPropertyChanged(nameof(CarId));
+        }
+    }
+    private string make;
+    public string Make
+    {
+        get => make;
+        set
+        {
+            if (value == make) return;
+            make = value;
+            OnPropertyChanged(nameof(Make));
+        }
+    }
+    private string color;
+    public string Color
+    {
+        get => color;
+        set
+        {
+            if (value == color) return;
+            color = value;
+            OnPropertyChanged(nameof(Color));
+        }
+    }
+    private string petName;
+    public string PetName
+    {
+        get => petName;
+        set
+        {
+            if (value == petName) return;
+            petName = value;
+            OnPropertyChanged(nameof(PetName));
+        }
+    }
+    public event PropertyChangedEventHandler PropertyChanged;
+    /// <summary> Вспомогательный метод индицирования события изменения свойства </summary>
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        //индицирование события со строкой, указывающей свойство, которое было изменено и нуждается в обновлении
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //обновление всех привязанных свойств объекта
+        //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(string.Empty));
     }
 }
 
