800fd47a753df92fca22fbe56f4f1c216e6fc85e
kanadeiar <kanadeiar@gmail.com>
2022-09-27 13:57:27 +0300

    clear wpf

--- SimpleSlnDir/SimpleWpf/MainWindow.xaml.cs
+++ SimpleSlnDir/SimpleWpf/MainWindow.xaml.cs
@@ -16,41 +16,9 @@ namespace SimpleWpf;
 
 public partial class MainWindow : Window
 {
-    readonly IList<Inventory> cars = new ObservableCollection<Inventory>(); //наблюдаемая коллекция
-
     public MainWindow()
     {
         InitializeComponent();
-        cars.Add(new Inventory
-        {
-            CarId = 1,
-            Make = "УАЗ",
-            Color = "Белый",
-            PetName = "Буханка",
-        });
-        cars.Add(new Inventory
-        {
-            CarId = 2,
-            Make = "ЗАЗ",
-            Color = "Коричневый",
-            PetName = "Большая"
-        });
-        comboBoxCars.ItemsSource = cars;
-    }
-    private void ButtonChangeColor_OnClick(object sender, RoutedEventArgs e)
-    {
-        cars.First<Inventory>(x => x.CarId == ((Inventory)comboBoxCars.SelectedItem)?.CarId).Color = "Pink";
-    }
-    private void buttonAddCar_Click(object sender, RoutedEventArgs e)
-    {
-        var maxCount = cars?.Max(x => x.CarId) ?? 0;
-        cars.Add(new Inventory
-        {
-            CarId = maxCount + 1,
-            Color = "Желтый",
-            Make = "ВАЗ",
-            PetName = "Копейка",
-        });
     }
 }
 
