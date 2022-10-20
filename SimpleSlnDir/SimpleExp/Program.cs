66b5f96ce61e2dbe39aa419971bfe5ce632a8b3c
kanadeiar <kanadeiar@gmail.com>
2022-10-20 15:25:59 +0300

    подготовка к .net 7

---
+++ SimpleSlnDir/SimpleExp/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Globalization;
+using System;
+using System.Numerics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+
+        Console.WriteLine("Press any key");
+        var _ = Console.ReadKey();
+    }
+}
