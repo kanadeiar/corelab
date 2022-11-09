5dbf7df865249c4558ad95266f040826b5caef66
kanadeiar <kanadeiar@gmail.com>
2022-11-09 09:59:23 +0300

    new math feature

--- SimpleSlnDir/Console/Program.cs
+++ SimpleSlnDir/Console/Program.cs
@@ -1,6 +1,19 @@
-﻿Console.WriteLine("Console application");
+﻿using System.Numerics;
 
+Console.WriteLine("Console application");
+
+var res = Sample.Add(1, 6);
+
+Console.WriteLine(res);
 
 
 Console.WriteLine("Press any key...");
 var _ = Console.ReadKey();
+
+public class Sample
+{
+    public static T Add<T>(T one, T two) where T : INumber<T>
+    {
+        return one * two;
+    }
+}
