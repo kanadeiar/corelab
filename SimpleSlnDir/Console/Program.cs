966da4aa0687b2c9b28fed7282656f0f12b54539
kanadeiar <kanadeiar@gmail.com>
2022-11-10 20:47:33 +0300

    generics

--- SimpleSlnDir/Console/Program.cs
+++ SimpleSlnDir/Console/Program.cs
@@ -2,18 +2,27 @@
 
 Console.WriteLine("Console application");
 
-var res = Sample.Add(1, 6);
-
-Console.WriteLine(res);
-
+for (byte i = 0; i <= 10; i++)
+{
+    Console.WriteLine($"{i} - {(Sample.IsSimple(i) ? "Простое" : "Сложное")}");
+}
 
 Console.WriteLine("Press any key...");
 var _ = Console.ReadKey();
 
-public class Sample
+public static class Sample
 {
     public static T Add<T>(T one, T two) where T : INumber<T>
     {
         return one * two;
+    }
+    public static bool IsSimple<T>(T n) where T : IBinaryInteger<T>
+    {
+        var k = (T)Convert.ChangeType(2, typeof(T));
+        while (k * k <= n && n % k != default(T))
+        {
+            k++;
+        }
+        return (k * k > n);
     }
 }
