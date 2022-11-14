756326f73b881e251f5688dfce586fedb7db3026
kanadeiar <kanadeiar@gmail.com>
2022-11-14 17:44:21 +0300

    wpf generic host

---
+++ SimpleSlnDir/HostWpf/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace HostWpf;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private IHost _host;
+
+    public App()
+    {
+        _host = new HostBuilder()
+            .ConfigureServices((context, services) =>
+            {
+                services.AddSingleton<MainWindow>();
+            })
+            .Build();
+    }
+    private async void Application_Startup(object sender, StartupEventArgs e)
+    {
+        await _host.StartAsync();
+
+        var mainWindow = _host.Services.GetService<MainWindow>();
+        mainWindow?.Show();
+    }
+    private async void Application_Exit(object sender, ExitEventArgs e)
+    {
+        using (_host)
+        {
+            await _host.StopAsync(TimeSpan.FromSeconds(5));
+        }
+    }
+}
+
+
+
