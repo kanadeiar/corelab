e18c758247ad6a24b6704e61b4fce0e5bfe8dfc8
kanadeiar <kanadeiar@gmail.com>
2022-11-24 17:44:59 +0300

    Пользовательский элемент

--- SimpleSlnDir/Wpf/Controls/NumberControl.xaml.cs
+++ SimpleSlnDir/Wpf/Controls/NumberControl.xaml.cs
@@ -2,26 +2,12 @@ namespace Wpf.Controls;
 
 public partial class NumberControl : UserControl
 {
-    public static readonly DependencyProperty TitleProperty =
-        DependencyProperty.Register("Title",
-        typeof(string),
-        typeof(NumberControl),
-        new PropertyMetadata(default(string)));
-    [Description("Заголовок")]
-    public string Title
-    {
-        get => (string)GetValue(TitleProperty);
-        set => SetValue(TitleProperty, value);
-    }
-
     public static readonly DependencyProperty CurrentNumberProperty =
         DependencyProperty.Register("CurrentNumber",
         typeof(int),
         typeof(NumberControl),
         new UIPropertyMetadata(0, new PropertyChangedCallback(CurrentNumberChanged)),
         new ValidateValueCallback(ValidateCurrentNumber));
-    public static bool ValidateCurrentNumber(object value) =>
-        Convert.ToInt32(value) >= 0 && Convert.ToInt32(value) <= 500;
     private static void CurrentNumberChanged(DependencyObject depObj,
         DependencyPropertyChangedEventArgs args)
     {
@@ -29,6 +15,9 @@ public partial class NumberControl : UserControl
         var theLabel = c.LabelNumber;
         theLabel.Content = args.NewValue.ToString();
     }
+    public static bool ValidateCurrentNumber(object value) =>
+        Convert.ToInt32(value) >= 0 && Convert.ToInt32(value) <= 300;
+    [Description("Текущий номер")]
     public int CurrentNumber
     {
         get => (int)GetValue(CurrentNumberProperty);
