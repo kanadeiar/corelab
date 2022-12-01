6752c92cf1efc0c12eb1272ca09d9b85659cf526
kanadeiar <kanadeiar@gmail.com>
2022-12-01 12:50:39 +0300

    Очистка

--- SimpleSlnDir/Console/Program.cs
+++ SimpleSlnDir/Console/Program.cs
@@ -1,16 +1,3 @@
 ﻿
-var baseVal = default(int);
-do
-{
-    Console.WriteLine("Input base:> ");
-} while (!int.TryParse(Console.ReadLine(), out baseVal));
-var heightVal = default(int);
-do
-{
-    Console.WriteLine("Input height:> ");
-} while (!int.TryParse(Console.ReadLine(), out heightVal));
-
-var areaVal = baseVal * heightVal / 2;
-Console.WriteLine($"area: {areaVal}");
 
 var _ = Console.ReadKey();
