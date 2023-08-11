05590b471f476fccaeab496836db70a94c11b324
kanadeiar <kanadeiar@gmail.com>
2023-08-11 17:04:01 +0300

    Небольшие изменения

--- SimpleSlnDir/ConsoleApp/Sample.cs
+++ SimpleSlnDir/ConsoleApp/Sample.cs
@@ -1,35 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp
+﻿namespace ConsoleApp
 {
     public class Sample
     {
-        public static async Task<IEnumerable<string>> GetPages(IEnumerable<string> urls, CancellationToken token = default,
-            IProgress<float>? progress = default)
+        public async Task<int> AddMethod(int value)
         {
-            var pages = new List<string>();
-            var current = 0.0f;
-            var step = 100.0f / urls.Count();
-            var us = urls as string[] ?? urls.ToArray();
-            foreach (var url in us.ToArray())
-            {
-                progress?.Report(current += step);
-                token.ThrowIfCancellationRequested();
-                var task = GetPage(url, token);
-                pages.Add(await task);
-            }
-            return pages;
-        }
-
-        public static async Task<string> GetPage(string url, CancellationToken token = default)
-        {
-            var client = new HttpClient();
-            var page = await client.GetStringAsync(url, token);
-            return page;
+            var val = value + 1;
+            await Task.Delay(500);
+            await Task.Delay(1000);
+            return val;
         }
     }
 }
--- SimpleSlnDir/ConsoleAppTests/SampleTests.cs
+++ SimpleSlnDir/ConsoleAppTests/SampleTests.cs
@@ -4,12 +4,6 @@ namespace ConsoleAppTests
 {
     public class SampleTests
     {
-        [Fact]
-        public async Task GetPage_Correct_ShouldBeOk()
-        {
-            var x = await Sample.GetPage(@"https://ya.ru");
-            var len = x.Length;
-            Assert.NotEqual(0, len);
-        }
+
     }
 }
