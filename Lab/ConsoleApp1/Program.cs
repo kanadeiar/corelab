3257f9837945e2d15d3d48b3b25c28c423fa4d34
kanadeiar <kanadeiar@gmail.com>
2023-09-23 11:51:58 +0300

    Новое приложение

---
+++ Lab/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ConsoleApp1;
+
+static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Опытное приложение работы с базой данных");
+
+
+
+        Console.WriteLine("Нажмиту любую кнопку ...");
+        Console.ReadKey();
+    }
+}
+
+
+
