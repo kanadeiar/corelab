c3ae8b54d39e0749e74dd595a21a9a116d743bfb
kanadeiar <kanadeiar@gmail.com>
2023-11-26 19:55:00 +0300

    Завершено

--- Lab/ConsoleApp1/GeneratePrimes/Generate.cs
+++ Lab/ConsoleApp1/GeneratePrimes/Generate.cs
@@ -77,44 +77,4 @@ public class Generate
         }
         return count;
     }
-
-    //var s = maxValue + 1;
-        //var f = new bool[s];
-        //int i;
-
-        //for (i = 0; i < s; i++)
-        //{
-        //    f[i] = true;
-        //}
-        //f[0] = f[1] = false;
-
-        //int j;
-        //for (i = 0; i < Math.Sqrt(s) + 1; i++)
-        //{
-        //    if (f[i])
-        //    {
-        //        for (j = 2 * i; j < s; j += i)
-        //        {
-        //            f[j] = false;
-        //        }
-        //    }
-        //}
-        
-        //var count = 0;
-        //for (i = 0; i < s; i++)
-        //{
-        //    if (f[i])
-        //        count++;
-        //}
-        //var primes = new int[count];
-
-        //for (i = 0, j = 0; i < s; i++)
-        //{
-        //    if (f[i])
-        //        primes[j++] = i;
-        //}
-
-        //return primes;
-
-    
 }
