f2b00189f8d4f443b8c822d4bb69bc01ffe9f528
kanadeiar <kanadeiar@gmail.com>
2023-12-16 11:20:18 +0300

    Начальная версия

---
+++ Lab/ConsoleApp1.Tests/Class/PrimePrinterTests.cs
@@ -0,0 +1,61 @@
+﻿using ConsoleApp1.Class;
+
+namespace ConsoleApp1.Tests.Class;
+
+[TestFixture]
+public class PrimePrinterTests
+{
+    [Test]
+    public void TestPrintPrimes()
+    {
+        var writer = new StringWriter();
+        Console.SetOut(writer);
+        var printer = new PrimePrinter();
+
+        printer.PrintPrimes();
+
+        var actuals = writer.ToString();
+        StringAssert.Contains("The First 1000 Prime Numbers --- Page 1", actuals);
+    }
+
+    [Test]
+    public void TestPrintPrimesFirstValues()
+    {
+        var writer = new StringWriter();
+        Console.SetOut(writer);
+        var printer = new PrimePrinter();
+
+        printer.PrintPrimes();
+
+        var actuals = writer.ToString();
+        StringAssert.Contains("2       107       211       315", actuals);
+    }
+
+    [Test]
+    public void TestPrintPrimesValues()
+    {
+        var writer = new StringWriter();
+        Console.SetOut(writer);
+        var printer = new PrimePrinter();
+
+        printer.PrintPrimes();
+
+        var actuals = writer.ToString();
+        StringAssert.Contains("3       109       213       317", actuals);
+        StringAssert.Contains("417       519       621       721", actuals);
+        StringAssert.Contains("823       925      1027      1129", actuals);
+    }
+
+    [Test]
+    public void TestPrintPrimesLastValues()
+    {
+        var writer = new StringWriter();
+        Console.SetOut(writer);
+        var printer = new PrimePrinter();
+
+        printer.PrintPrimes();
+
+        var actuals = writer.ToString();
+        StringAssert.Contains("1735      1835      1937      2039", actuals);
+    }
+}
---
+++ Lab/ConsoleApp1/Class/PrimePrinter.cs
@@ -0,0 +1,75 @@
+﻿namespace ConsoleApp1.Class;
+
+public class PrimePrinter
+{
+    public void PrintPrimes()
+    {
+        int M = 1000;
+        int RR = 50;
+        int CC = 4;
+        int ORDMAX = 30;
+        int[] P = new int[M + 1];
+        int C;
+        int J = 1;
+        int K = 1;
+        bool JPRIME;
+        int ORD = 2;
+        int SQUARE = 9;
+        int N;
+        int[] MULT = new int[ORDMAX + 1];
+
+        P[1] = 2;
+
+        while (K < M)
+        {
+            do
+            {
+                J += 2;
+                if (J == SQUARE)
+                {
+                    ORD++;
+                    SQUARE = P[ORD] * P[ORD];
+                    MULT[ORD - 1] = J;
+                }
+                N = 2;
+                JPRIME = true;
+                while (N < ORD && JPRIME)
+                {
+                    while (MULT[N] < J)
+                    {
+                        MULT[N] += MULT[N] + P[N] * 2;
+                    }
+                    if (MULT[N] == J)
+                    {
+                        JPRIME = false;
+                    }
+                    N++;
+                }
+            } while (!JPRIME);
+            K++;
+            P[K] = J;
+        }
+
+        int PAGENUMBER = 1;
+        int PAGEOFFSET = 1;
+        int ROWOFFSET;
+        while (PAGEOFFSET <= M)
+        {
+            Console.WriteLine("The First " + M + " Prime Numbers --- Page " + PAGENUMBER);
+            Console.WriteLine();
+            for (ROWOFFSET = PAGEOFFSET; ROWOFFSET < PAGEOFFSET + RR; ROWOFFSET++)
+            {
+                for (C = 0; C < CC; C++)
+                {
+                    if (ROWOFFSET + C * RR <= M)
+                    {
+                        Console.Write("{0,10}", P[ROWOFFSET + C * RR]);
+                    }
+                }
+                Console.WriteLine();
+            }
+            PAGENUMBER++;
+            PAGEOFFSET += RR * CC;
+        }
+    }
+}
