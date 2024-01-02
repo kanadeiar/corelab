4f9c6af710f66c7e30a3582953fae977c627ebc4
kanadeiar <kanadeiar@gmail.com>
2024-01-02 22:36:55 +0300

    + Фибоначчи

---
+++ Lab/ConsoleApp1.Tests/Fibonacci/FibonacciTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+
+namespace ConsoleApp1.Tests.Fibonacci;
+
+[TestFixture]
+internal class FibonacciTests
+{
+    [TestCase(0, 0)]
+    [TestCase(1, 1)]
+    [TestCase(2, 1)]
+    [TestCase(3, 2)]
+    [TestCase(4, 3)]
+    [TestCase(5, 5)]
+    public void TestFibonacci(int value, int expected)
+    {
+        var fib = new Fibonacci();
+        Assert.AreEqual(expected, fib.Fib(value));
+    }
+}
+
+public class Fibonacci
+{
+    public T Fib<T>(T n)
+        where T : INumber<T>
+    {
+        if (n == T.Zero) return T.Zero;
+        if (n == T.One) return T.One;
+        return Fib(n - T.One) + Fib(n - (T.One + T.One));
+    }
+}
