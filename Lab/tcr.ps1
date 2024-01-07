0fb3a8b219f1123e7ac93336e08a1d51bd9c68c7
kanadeiar <kanadeiar@gmail.com>
2024-01-07 23:18:38 +0300

    Исправлено

--- Lab/tcr.ps1
+++ Lab/tcr.ps1
@@ -2,16 +2,16 @@
 
 Function Build-Failed{
     Write-Host "### Building the solution"
-    Invoke-Command "dotnet build TCR.sln"
+    Invoke-Command "dotnet build"
     return $LASTEXITCODE -eq 1
 }
 function Tests-Run{
     Write-Host "### Running tests"
-    Invoke-Expression -Command: "dotnet test TCR.sln" | Tee-Object -Variable output | Write-Host
+    Invoke-Expression -Command: "dotnet test" | Tee-Object -Variable output | Write-Host
     return $output
 }
 Function Tests-Pass($testOutput){
-    return $testOutput -Match "Test Run Successful"
+    return $testOutput -Match "Пройден!"
 }
 Function Single-NotImplementedException($testOutput){
     $count = ([regex]::Matches($testOutput, "System.NotImplementedException: The method or operation is not implemented." )).count
@@ -23,7 +23,7 @@ Function Commit{
 }
 Function Revert{
     # Invoke-Command
-    Invoke-Command "git checkout HEAD -- src"
+    Invoke-Command "git reset --hard HEAD"
 }
 
 Function Invoke-Command($command){
@@ -31,19 +31,21 @@ Function Invoke-Command($command){
 }
 
 if(Build-Failed){
-    Write-Host "### Build failed. No change."
+    Write-Host "### TCR Build failed. No change."
     return
 }
-Write-Host "### Build Passed"
+Write-Host "### TCR Build Passed"
 $testOutput = Tests-Run
 if(Single-NotImplementedException $testOutput){
     Write-Host "### A single NotImplementedException is allowed. No Change."
     return
 }
-if(Tests-Pass $testOutput){
-    Write-Host "### Tests Passed. Commiting Changes."
+Write-Host "result $lastexitcode" 
+if ($lastexitcode -eq 0){
+#if(Tests-Pass $testOutput){
+    Write-Host "### TCR Tests Passed. Commiting Changes."
     Commit
 } else {
-    Write-Host "### Tests Failed. Reverting..."
+    Write-Host "### TCR Tests Failed. Reverting..."
     Revert
 }
