d24db0e79ac5903c5ddcab2a119b6b5051c5711c
kanadeiar <kanadeiar@gmail.com>
2024-01-10 22:28:25 +0300

    Перед рефакторингом

---
+++ Lab/ConsoleApp1/Refactoring.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1;
+
+public class Refactoring
+{
+    public int Gamma(int inputVal, int quantity, int yearToDate)
+    {
+        var importantValue1 = (inputVal * quantity) + delta();
+        var importantValue2 = (inputVal * yearToDate) + 100;
+        if (yearToDate - importantValue1 > 100)
+        {
+            importantValue1 -= 20;
+        }
+        var importantValue3 = importantValue1 * 7;
+        return importantValue3 - 2 * importantValue2;
+    }
+
+    private int delta()
+    {
+        return 1;
+    }
+}
