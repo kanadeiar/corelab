6e0bd2e0ac8ace1bdaa09dea2d41c0e837a510d2
kanadeiar <kanadeiar@gmail.com>
2024-01-11 21:42:41 +0300

    Capped + Int Range

---
+++ Lab/ConsoleApp1/Refactoring.cs
@@ -0,0 +1,68 @@
+﻿namespace ConsoleApp1;
+
+
+public class IntRange
+{
+    private int _low;
+    public int Low
+    {
+        get => _low;
+        set => _low = value;
+    }
+
+    private int _high;
+    public int High
+    {
+        get => _high;
+        set => _high = value;
+    }
+
+    protected IntRange() { }
+    public IntRange(int low, int high)
+    {
+        create(low, high);
+    }
+    protected void create(int low, int high)
+    {
+        _low = low;
+        _high = high;
+    }
+
+    public bool Includes(int value)
+    {
+        return _low <= value && value <= _high;
+    }
+
+    public void Grow(int factor)
+    {
+        High *= factor;
+    }
+}
+
+public class CappedRange : IntRange
+{
+    private int _cap;
+
+    public int Cap
+    {
+        get => _cap;
+        set => _cap = value;
+    }
+
+    public new int High
+    {
+        get => Math.Min(_cap, base.High);
+    }
+
+    public CappedRange(int low, int high, int cap)
+    {
+        create(low, high);
+        _cap = cap;
+    }
+}
+
+public class Refactoring
+{
+
+
+}
---
+++ Lab/Tests/ConsoleApp1.Tests/IntRangeTests.cs
@@ -0,0 +1,60 @@
+﻿namespace ConsoleApp1.Tests;
+
+public class IntRangeTests
+{
+    [Fact]
+    public void TestLow()
+    {
+        var range = new IntRange(1, 10);
+
+        range.Low = 10;
+        var actual = range.Low;
+
+        Assert.Equal(10, actual);
+    }
+
+    [Fact]
+    public void TestHigh()
+    {
+        var range = new IntRange(1, 10);
+
+        range.High = 20;
+        var actual = range.High;
+
+        Assert.Equal(20, actual);
+    }
+
+    [Fact]
+    public void TestIncludes()
+    {
+        var range = new IntRange(1, 10);
+        
+        Assert.True(range.Includes(5));
+        Assert.False(range.Includes(0));
+    }
+
+    [Fact]
+    public void TestGrow()
+    {
+        var range = new IntRange(1, 10);
+
+        range.Grow(2);
+
+        Assert.Equal(10 * 2, range.High);
+    }
+}
+
+public class CappedRangeTests
+{
+    [Fact]
+    public void TestCap()
+    {
+        var range = new CappedRange(1, 10, 5);
+
+        range.Cap = 7;
+        
+        Assert.Equal(1, range.Low);
+        Assert.Equal(7, range.Cap);
+        Assert.Equal(7, range.High);
+    }
+}
