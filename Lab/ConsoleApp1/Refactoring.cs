5683567fe03cc4e994d49919bc50a98587164c10
kanadeiar <kanadeiar@gmail.com>
2024-01-14 19:28:19 +0300

    Декомпозиция условного оператора

--- Lab/ConsoleApp1/Refactoring.cs
+++ Lab/ConsoleApp1/Refactoring.cs
@@ -4,6 +4,29 @@ namespace ConsoleApp1;
 
 public class Refactoring
 {
+    public int Calc(DateTime date)
+    {
+        int charge;
+        if (notSummer(date))
+            charge = summerCharge();
+        else
+            charge = winterCharge();
 
+        return charge;
+    }
 
+    private static bool notSummer(DateTime date)
+    {
+        return date.Month >= 6 && date.Month <= 8;
+    }
+
+    private static int summerCharge()
+    {
+        return 1 * 232 + 1988;
+    }
+
+    private static int winterCharge()
+    {
+        return 3 * 60;
+    }
 }
