5bb988ea83a61b919122f20cbe948eb0b31a1b87
kanadeiar <kanadeiar@gmail.com>
2024-01-15 08:09:57 +0300

    Очистка

--- Lab/Tests/ConsoleApp1.Tests/RefactoringTests.cs
+++ Lab/Tests/ConsoleApp1.Tests/RefactoringTests.cs
@@ -7,26 +7,6 @@ public class RefactoringTests
     {
 
     }
-
-    [Fact]
-    public void TestCalc()
-    {
-        var ref1 = new Refactoring();
-
-        var actual = ref1.Calc(new DateTime(1988, 7, 1));
-
-        Assert.Equal(1 * 232 + 1988, actual);
-    }
-
-    [Fact]
-    public void TestCalc_WhenNotRange()
-    {
-        var ref1 = new Refactoring();
-
-        var actual = ref1.Calc(new DateTime(1988, 3, 1));
-
-        Assert.Equal(3 * 60, actual);
-    }
 }
 
 
