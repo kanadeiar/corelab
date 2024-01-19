2b901d22c6b6362c342ffd3a7813f7618ddbee48
kanadeiar <kanadeiar@gmail.com>
2024-01-20 00:50:55 +0300

    Завершение

--- Lab/ConsoleApp1/Refactoring.cs
+++ Lab/ConsoleApp1/Refactoring.cs
@@ -5,45 +5,6 @@ public class Refactoring
 
 }
 
-public abstract class Employee
-{
-    public int GetSalary(int days)
-    {
-        var salary = Salary();
-        var basic = salary * days;
-        var prem = Prem(basic);
-        return basic + prem;
-    }
-
-    protected abstract int Prem(int basic);
-    protected abstract int Salary();
-}
-
-public class Engineer : Employee
-{
-    protected override int Prem(int basic)
-    {
-        return basic * 2;
-    }
-
-    protected override int Salary()
-    {
-        return 100;
-    }
-}
-
-public class Managet : Employee
-{
-    protected override int Prem(int basic)
-    {
-        return basic * 3;
-    }
-
-    protected override int Salary()
-    {
-        return 200;
-    }
-}
 
 
 
