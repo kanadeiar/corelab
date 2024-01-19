9a5796ef6b5c25013bc2770d2a34516cc457376c
kanadeiar <kanadeiar@gmail.com>
2024-01-19 23:25:44 +0300

    Извлечение интерфейса

--- Lab/ConsoleApp1/Program.cs
+++ Lab/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        
+
 
 
         Console.WriteLine("Нажмиту любую кнопку ...");
--- Lab/ConsoleApp1/Refactoring.cs
+++ Lab/ConsoleApp1/Refactoring.cs
@@ -2,10 +2,36 @@
 
 public class Refactoring
 {
+    public double Charge(IBillable emp, int days)
+    {
+        var bas = emp.GetRate() * days;
+        if (emp.HasSpecialSkill())
+        {
+            return bas * 1.05;
+        }
 
+        return bas;
+    }
 }
 
+public interface IBillable
+{
+    int GetRate();
+    bool HasSpecialSkill();
+}
 
+public class Employee : IBillable
+{
+    public int GetRate()
+    {
+        return 10;
+    }
+
+    public bool HasSpecialSkill()
+    {
+        return true;
+    }
+}
 
 
 
