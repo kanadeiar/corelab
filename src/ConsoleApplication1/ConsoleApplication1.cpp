2ca172c73d743c277297bcff32afe6241ade9d31
kanadeiar <kanadeiar@gmail.com>
2024-02-09 20:51:25 +0300

    C++ condole

---
+++ src/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    Console::WriteLine("Опытное приложение");
+
+
+
+	Console::WriteLine("Нажмите любую кнопку для завершения ...");
+	Console::ReadKey();
+    return 0;
+}
