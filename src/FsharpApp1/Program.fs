7255d4a03978e8f4de6d4c626fdbf97c9610b2ab
kanadeiar <kanadeiar@gmail.com>
2024-02-24 01:55:45 +0300

    Асинхронность

--- src/FsharpApp1/Program.fs
+++ src/FsharpApp1/Program.fs
@@ -1,7 +1,24 @@
 ﻿open System
+open System.IO
+open Persons
+open Shapes
+open Collections
 
 printfn "Опытное приложение"
 
+task {
+    let! text = File.ReadAllTextAsync "hello.txt" |> Async.AwaitTask
+    Async.Sleep 1000 |> Async.RunSynchronously
+    printfn "%s" text
+} |> ignore
+
+
+let emp = Employee("name", 12, "one")
+
+async {
+    let! res = emp.TestAsync "andrey"
+    printfn "%s" res
+} |> Async.RunSynchronously
 
 
 printfn "\nНажмите любую кнопку для завершения ..."
