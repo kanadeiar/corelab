b4f65639295070fad41b0f1b312a42002c9d7ee6
kanadeiar <kanadeiar@gmail.com>
2025-08-23 23:06:33 +0300

    Новшества

--- src/MauiApp1/Views/QuestionPage.xaml.cs
+++ src/MauiApp1/Views/QuestionPage.xaml.cs
@@ -6,49 +6,66 @@ namespace MauiApp1.Views;
 
 public partial class QuestionPage : ContentPage
 {
-	public QuestionPage()
+    public QuestionPage()
 	{
 		InitializeComponent();
 	}
 
-    private async void Button_OnClicked(object? sender, EventArgs e)
+    private void Button_OnClicked(object? sender, EventArgs e)
     {
         try
         {
-            var address = "";
-            var port = 123;
-            var useSsl = false;
-            var login = "";
-            var password = "";
-
-            var from = "";
-            var to = "";
-            var subject = "";
-            
             var name = NameEntry.Text;
             var idea = IdeaEditor.Text;
-            var text = $"Имя автора идеи: {name} идея: {idea}";
+            if (string.IsNullOrEmpty(name))
+            {
+                DisplayAlert("Недостаточно данных", "Пожалуйста, введите свое имя", "OK");
+                SendButton.Text = "Отправить";
+                SendButton.IsEnabled = true;
+                return;
+            }
 
-            //var tMessage = new MailMessage(from, to)
-            //{
-            //    Subject = subject,
-            //    Body = text,
-            //};
-            //var client = new SmtpClient(address, port)
-            //{
-            //    EnableSsl = useSsl,
-            //    Credentials = new NetworkCredential(login, password),
-            //};
+            if (string.IsNullOrEmpty(idea))
+            {
+                DisplayAlert("Недостаточно данных", "Введите описание своей идеи", "OK");
+                SendButton.Text = "Отправить";
+                SendButton.IsEnabled = true;
+                return;
+            }
 
-            //client.Send(tMessage);
+            var address = "smtp.yandex.ru";
+            var port = 587;
+            var useSsl = true;
+            var login = "kanadeiar";
+            var password = "wzendjlmrxpxmnwn";
 
-            await DisplayAlert("Выполнено", "Сообщение отправлено", "OK");
+            var from = "kanadeiar@yandex.ru";
+            //var to = "kanadeiar@gmail.com";
+            var to = "nikitin@nsoil.ru";
+            var subject = "Тестирование";
+
+            var text = $"Имя автора идеи: {name}\nОписание идеи:\n{idea}";
+
+            var tMessage = new MailMessage(from, to)
+            {
+                Subject = subject,
+                Body = text,
+            };
+            var client = new SmtpClient(address, port)
+            {
+                EnableSsl = useSsl,
+                Credentials = new NetworkCredential(login, password),
+            };
+
+            client.Send(tMessage);
+
+            DisplayAlert("Выполнено", "Сообщение отправлено", "OK");
             NameEntry.Text = string.Empty;
             IdeaEditor.Text = string.Empty;
         }
         catch (Exception ex)
         {
-            await DisplayAlert("Ошибка", "Не удалось отправить сообщение. Ошибка: " + ex, "OK");
+            DisplayAlert("Ошибка", "Не удалось отправить сообщение. Ошибка: " + ex, "OK");
         }
     }
 }
